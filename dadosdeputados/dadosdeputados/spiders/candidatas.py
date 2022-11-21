95152ab16b76e659fb4c2accbc1dace22e02a3a2
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2022-11-20 22:49:41 -0300

    add project

---
+++ dadosdeputados/dadosdeputados/spiders/candidatas.py
@@ -0,0 +1,21 @@
+import scrapy
+
+from ..utils import Utils
+
+class QuotesSpider(scrapy.Spider):
+    name = "candidatas"
+
+    def start_requests(self):
+        deps_file = open("../lista_deputadas.txt", "r")
+
+        deps_urls = deps_file.read().splitlines()
+
+        for url in deps_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        main_response= Utils(response)
+
+        main_data = main_response.main(gender="F")
+
+        yield main_data
